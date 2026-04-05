ae3a7322726097b552395b0d63c8364530e4350f
geralt.sun <geralt.sun@ly.com>
2026-04-05 09:26:55 +0800

    更新关于页面的样式，添加边框并调整分类名称颜色；修正时间戳默认值

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,6 @@
 
 export default new Map([
-["content/writing-system/1-writing-methodology.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fwriting-system%2F1-writing-methodology.mdx&astroContentModuleFlag=true")],
+["content/insights/1-harness-engineering.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Finsights%2F1-harness-engineering.mdx&astroContentModuleFlag=true")],
 ["content/skills/1-veo.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fskills%2F1-veo.mdx&astroContentModuleFlag=true")],
-["content/insights/1-harness-engineering.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Finsights%2F1-harness-engineering.mdx&astroContentModuleFlag=true")]]);
+["content/writing-system/1-writing-methodology.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fwriting-system%2F1-writing-methodology.mdx&astroContentModuleFlag=true")]]);
 		
